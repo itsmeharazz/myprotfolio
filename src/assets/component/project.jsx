169ccbfbd4319652a3cf5ab1ceed297f246c2066
[Meharazul islam] <[itsmeharazz]>
2024-10-26 00:17:43 +0600

    add project

--- src/assets/component/project.jsx
+++ src/assets/component/project.jsx
@@ -1,21 +1,17 @@
 import { Element } from "react-scroll"
+import ProjectHeader from "./project/projectHeader"
+import Projectcard from "./project/projectcard"
 function Project() {
   return (
      <Element name="project-section">
     <div>
-     <div className="projectTop w-full py-4 bg-[#ea4343]">
+     <div className="projectTop w-full pt-5 bg-[#ea4343]">
      <div className="container w-full h-[10em] mx-auto ">
           <p>My Project</p>
           <h2 className="text-3xl my-4 font-semibold">Some of the projects that I have works in</h2>
-          <div className="projectnav">
-               <ul className="flex gap-9 text-white">
-                    <li>All </li>
-                    <li>Vanilla CSS </li>
-                    <li>React </li>
-                    <li>Laravel </li>
-               </ul>
-          </div>
+          <ProjectHeader/>
      </div>
+          <Projectcard />
      </div>
     </div>
      </Element>
--- src/assets/component/project/projectHeader.jsx
+++ src/assets/component/project/projectHeader.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react"
 
 function ProjectHeader() {
+  const projects = [
+    {
+      lable:'All',
+      img:'./WhatsApp Image 2024-05-19 at 11.50.52 PM.jpeg'
+    },
+    {
+      lable:'Vanila CSS',
+      img:'screencapture-behance-net-gallery-175939839-Portfolio-Website-Landing-Page-2023-08-10-23_25_32.png'
+    },
+    {
+      lable:'React',
+    img:'screencapture-lawfullondon-2024-05-19-23_05_45.png'
+    },
+    {
+      lable:'Laravel',
+     img:'./WhatsApp Image 2024-05-19 at 11.50.52 PM.jpeg'
+    },
+  ];
+  const[position, setPosition] = useState(0);
   return (
-    <div>projectHeader</div>
+    <div className="project">
+    <div className="flex flex-col gap-3 container max-w-md mb-12">
+      <ul className="bg-[#ea4343] px-4 rounded-xl flex items-center text-white">
+    {projects.map((tab, idx) =>(
+      <li className={`flex-1 font-medium text-center text-white text-lg py-6 relative before:absolute before:left-0 before:bottom-0 before:h-1 before:w-full before:bg-[#FFF] before:transition cursor-pointer ${position == idx ?"before:scale100 text-[#3c3c3c]":"before:scale-0"}`}
+        key={idx}
+        onClick={() => setPosition(idx)}>
+        {tab.lable}
+      </li>
+    ))}
+      </ul>
+    </div>
+     
+    </div>
   )
 }
 
