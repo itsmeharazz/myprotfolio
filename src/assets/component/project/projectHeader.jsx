8f68846ed804e87efcf65fa782dbeb96eca4e21a
[Meharazul islam] <[itsmeharazz]>
2024-10-26 23:19:49 +0600

    tray update project section

--- src/assets/component/project/projectHeader.jsx
+++ src/assets/component/project/projectHeader.jsx
@@ -1,22 +1,37 @@
-import { useState } from "react"
-
+import { Children, useState } from "react"
+import mordenhome from "../../image/css.png"
+import restaurant from "../../image/css_2.png"
+import mkted from "../../image/css_3.png"
+import hospital from "../../image/Laravel.png"
 function ProjectHeader() {
   const projects = [
     {
       lable:'All',
-      img:'./WhatsApp Image 2024-05-19 at 11.50.52 PM.jpeg'
+      img:mordenhome,
     },
     {
       lable:'Vanila CSS',
-      img:'screencapture-behance-net-gallery-175939839-Portfolio-Website-Landing-Page-2023-08-10-23_25_32.png'
+      Children:[
+        {
+          img:mordenhome,
+        },
+        {
+
+         img:restaurant,
+        },
+        {
+
+           img:mkted,
+        }
+      ]
     },
     {
       lable:'React',
-    img:'screencapture-lawfullondon-2024-05-19-23_05_45.png'
+    img:"",
     },
     {
       lable:'Laravel',
-     img:'./WhatsApp Image 2024-05-19 at 11.50.52 PM.jpeg'
+     img:hospital
     },
   ];
   const[position, setPosition] = useState(0);
@@ -25,7 +40,7 @@ function ProjectHeader() {
     <div className="flex flex-col gap-3 container max-w-md mb-12">
       <ul className="bg-[#ea4343] px-4 rounded-xl flex items-center text-white">
     {projects.map((tab, idx) =>(
-      <li className={`flex-1 font-medium text-center text-white text-lg py-6 relative before:absolute before:left-0 before:bottom-0 before:h-1 before:w-full before:bg-[#FFF] before:transition cursor-pointer ${position == idx ?"before:scale100 text-[#3c3c3c]":"before:scale-0"}`}
+      <li className={`flex-1 font-medium text-center text-white text-lg py-6 relative before:absolute before:left-0 before:bottom-0 before:h-1 before:w-full before:bg-[#FFF] before:transition cursor-pointer ${position == idx ?"before: scale100 text-[#3c3c3c]":"before:scale-0"}`}
         key={idx}
         onClick={() => setPosition(idx)}>
         {tab.lable}
@@ -33,6 +48,20 @@ function ProjectHeader() {
     ))}
       </ul>
     </div>
+      {/* <div className="bg-[#3c3c3c] w-full ">
+        <div className="container flex justify-between items-center gap-3 mx-auto">
+          <div className="card rounded-3xl w-[30%] h-[25em]  truncate my-6 relative">
+          <img src={projects[position].img} alt="" className="w-full"/>
+            <div className="bottom  w-full flex justify-between absolute left-0  bottom-0">
+               <button className="bg-[#3c3c3c] w-[45%] hover:text-black hover:border hover:border-[#ea4343] hover:bg-transparent px-5 pt-2 text-white" >Live Link</button>
+               <button className="bg-[#3c3c3c] w-[45%] hover:text-black hover:border hover:border-[#ea4343] hover:bg-transparent px-5 pt-2 text-white" >Githu Link</button>
+            </div>
+          </div>
+         
+          
+          
+        </div>
+      </div> */}
      
     </div>
   )
--- src/assets/component/project/projectcard.jsx
+++ src/assets/component/project/projectcard.jsx
@@ -1,51 +1,45 @@
-import { useState } from "react"
-
+// import { useState } from "react"
+import mordenhome from "../../image/css.png"
+import restaurant from "../../image/css_2.png"
+import mkted from "../../image/css_3.png"
+// import hospital from "../../image/Laravel.png"
 
 function Projectcard() {
      const projects =[
           {    
-               img:'./WhatsApp Image 2024-05-19 at 11.50.52 PM.jpeg'
+               img:mordenhome,
 
           },
           {    
-               img:'screencapture-lawfullondon-2024-05-19-23_05_45.png'
+               img:restaurant,
 
           },
           {    
-               img:'./WhatsApp Image 2024-05-19 at 11.50.52 PM.jpeg'
+               img:mkted,
 
           },
-          {    
-               img:'./WhatsApp Image 2024-05-19 at 11.50.52 PM.jpeg'
+          // {    
+          //      img:hospital,
 
-          },
+          // },
      ];
-     const[project] = useState(0);
+     // const[project] = useState(0);
   return (
     <div>
+
  <div className="bg-[#3c3c3c] w-full ">
         <div className="container flex justify-between items-center gap-3 mx-auto">
-          <div className="card rounded-3xl w-[30%] h-[25em]  truncate my-6 relative">
-            <img src={projects[project].img} alt="" className="w-full"/>
+        {projects.map((imgproject, projectpic)=>(
+
+          <div className="card rounded-3xl w-[27%] h-[25em]  truncate my-6 relative" key={projectpic}>
+            <img src={imgproject.img} alt="" className="w-full"/>
             <div className="bottom  w-full flex justify-between absolute left-0  bottom-0">
-               <button className="bg-[#3c3c3c] w-[45%] hover:text-black hover:border hover:border-[#ea4343] hover:bg-transparent px-5 pt-2 text-white" >Live Link</button>
-               <button className="bg-[#3c3c3c] w-[45%] hover:text-black hover:border hover:border-[#ea4343] hover:bg-transparent px-5 pt-2 text-white" >Githu Link</button>
+               <button className="bg-[#3c3c3c] w-[45%] hover:text-black hover:border hover:border-[#ea4343] hover:bg-[#ea4343] px-5 pt-2 text-white" >Live Link</button>
+               <button className="bg-[#3c3c3c] w-[45%] hover:text-black hover:border hover:border-[#ea4343] hover:bg-[#ea4343] px-5 pt-2 text-white" >Githu Link</button>
             </div>
           </div>
-          <div className="card rounded-3xl w-[30%] h-[25em] truncate my-6 relative">
-            <img src={projects[project].img} alt="" className="w-full"/>
-            <div className="bottom  w-full flex justify-between absolute left-0  bottom-0">
-               <button className="bg-[#3c3c3c] w-[45%] hover:text-black hover:border hover:border-[#ea4343] hover:bg-transparent px-5 pt-2 text-white" >Live Link</button>
-               <button className="bg-[#3c3c3c] w-[45%] hover:text-black hover:border hover:border-[#ea4343] hover:bg-transparent px-5 pt-2 text-white" >Githu Link</button>
-            </div>
-          </div>
-          <div className="card rounded-3xl w-[30%] h-[25em] truncate my-6 relative">
-            <img src={projects[project].img} alt="" className="w-full"/>
-            <div className="bottom  w-full flex justify-between absolute left-0  bottom-0">
-               <button className="bg-[#3c3c3c] w-[45%] hover:text-black hover:border hover:border-[#ea4343] hover:bg-transparent px-5 pt-2 text-white" >Live Link</button>
-               <button className="bg-[#3c3c3c] w-[45%] hover:text-black hover:border hover:border-[#ea4343] hover:bg-transparent px-5 pt-2 text-white" >Githu Link</button>
-            </div>
-          </div>
+        ))}
+         
           
           
         </div>
